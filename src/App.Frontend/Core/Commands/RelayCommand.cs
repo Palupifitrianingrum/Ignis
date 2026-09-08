ea14cc4e1e47cbd343623b630ff561f4c9989f0c
nael <natansimanjuntak17@gmail.com>
2026-09-09 05:09:52 +0700

    Membuat Folder Frontend

---
+++ src/App.Frontend/Core/Commands/RelayCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ignis.Frontend.Core.Commands
+{
+    internal class RelayCommand
+    {
+    }
+}
---
+++ src/App.Frontend/Features/Auth/Login/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ignis.Frontend.Features.Auth.Login
+{
+    internal class LoginViewModel
+    {
+    }
+}
